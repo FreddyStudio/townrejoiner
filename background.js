85c6f423b6749a47d76e9c0ebefb0177df237bab
Rio <FreddyStudioYT@gmail.com>
2025-04-11 18:25:10 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
-// Function to execute content script on specified tabs
 function checkAndClick() {
   const urlsToMonitor = [
     "https://test.pixel.horse/*",
     "https://event.pony.town/*",
+    "https://building.pony.town/*",
     "https://pony.town/*"
   ];
 
@@ -20,25 +20,21 @@ function checkAndClick() {
   });
 }
 
-// Set an alarm to run the checkAndClick function every 5 minutes
 chrome.alarms.create("rejoinGame", { periodInMinutes: 5 });
 
-// Add a listener for the alarm
 chrome.alarms.onAlarm.addListener(function(alarm) {
   if (alarm.name === "rejoinGame") {
     checkAndClick();
   }
 });
 
-// Initial check when the extension is installed or reloaded
 checkAndClick();
 
-// Listen for messages from content scripts
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'checkAutoJoinerStatus') {
     chrome.storage.local.get(['autoJoinerEnabled'], function(result) {
       sendResponse({ autoJoinerEnabled: result.autoJoinerEnabled !== false });
     });
-    return true; // Indicate that the response is asynchronous
+    return true; 
   }
 });
