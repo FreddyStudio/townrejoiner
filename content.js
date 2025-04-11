944baadf93e4eb04b00d0502669900bff4e7e8b9
Rio <FreddyStudioYT@gmail.com>
2025-04-11 18:25:22 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,4 +1,3 @@
-// Function to send a message to the background script to check the auto joiner status
 function checkAutoJoinerStatus() {
   chrome.runtime.sendMessage({ action: 'checkAutoJoinerStatus' }, function(response) {
     if (chrome.runtime.lastError) {
@@ -9,16 +8,15 @@ function checkAutoJoinerStatus() {
   });
 }
 
-// Function to click the Play button
 function clickPlayButton() {
   const playButton = document.querySelector('.btn.btn-lg.btn-success.text-ellipsis.flex-grow-1') ||
                      document.querySelector('.btn.btn-lg.btn-success.text-truncate.flex-grow-1') ||
-                     document.querySelector('.btn btn-lg btn-success text-truncate flex-grow-1');
+                     document.querySelector('.btn btn-lg btn-success text-truncate flex-grow-1'); 
   
   if (playButton) {
     playButton.click();
   }
 }
 
-// Check and click the Play button every 3 seconds
 setInterval(checkAutoJoinerStatus, 3000);
+
